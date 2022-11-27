9098424dd01e43c5f4a45f4d44ce3ab38d461b1a
AlimunNahar <alimunnaharmitu@gmail.com>
2022-11-28 03:21:33 +0600

    Buyer Route Created

---
+++ src/Routes/Routes/BuyerRoute.js
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthProvider";
+import useBuyer from "../../Hooks/useBuyer";
+import { Navigate, useLocation } from "react-router-dom";
+import Loading from "../../Pages/Shared/Loading/Loading";
+
+const BuyerRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const [isBuyer, isBuyerLoading] = useBuyer(user?.email);
+  const location = useLocation();
+
+  if (loading || isBuyerLoading) {
+    return <Loading />;
+  }
+
+  if (loading) {
+    return (
+      <div className="mx-auto my-60 w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-cyan-800"></div>
+    );
+  }
+
+  if (user && isBuyer) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+};
+
+export default BuyerRoute;
