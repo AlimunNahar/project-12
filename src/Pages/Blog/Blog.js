fd1517ed6a018f167c322ee83bf9ff788bb7d3ec
AlimunNahar <alimunnaharmitu@gmail.com>
2022-11-28 00:26:22 +0600

    creating blog route

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -3,15 +3,32 @@ import React from "react";
 const Blog = () => {
   return (
     <div>
-      <div>
-        <ul className="steps steps-vertical">
-          <li className="step hover:step-primary">Register</li>
-          <li className="step hover:step-primary">Choose plan</li>
-          <li className="step hover:step-primary">Purchase</li>
-          <li className="step hover:step-primary">Receive Product</li>
-        </ul>
+      <h2 className="divider text-cyan-500 text-3xl my-12">
+        Question & Answers
+      </h2>
+      <div className="block lg:flex">
+        <div className="text-center py-3">
+          <ul className="steps steps-horizontal lg:steps-vertical text-xl my-6 ">
+            <li className="step hover:step-primary  hover:text-cyan-400">
+              Manage State
+            </li>
+            <li className="step hover:step-primary hover:text-cyan-400">
+              Prototypical Inheritance
+            </li>
+            <li className="step hover:step-primary hover:text-cyan-400">
+              Unit Test
+            </li>
+            <li className="step hover:step-primary hover:text-cyan-400">
+              React vs Angular vs Vue
+            </li>
+          </ul>
+        </div>
+        <div className="w-full text-left lg:w-9/12">
+          <div>
+            <h2></h2>
+          </div>
+        </div>
       </div>
-      <div></div>
     </div>
   );
 };
